701ee1a50a3d3b236a2d13bcc4ef8c11cee1bd34
KoreanFubuki <145092552+KoreanFubuki@users.noreply.github.com>
2023-09-19 16:50:56 +0400

    Add project files.

---
+++ Exercise 7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        static void Main(string[] args)
+        {
+            WhileLoop();
+            Console.WriteLine("\n");
+            ForLoop();
+
+            Console.Read();
+        }
+        public static void ForLoop()
+        {
+            for (int num = -3; num <= 3; num++)
+            {
+                Console.WriteLine(num);
+            }
+        } 
+
+        public static void WhileLoop()
+        {
+            int numm = 3;
+            while (numm >= -3)
+            {
+                Console.WriteLine(numm);
+                numm--;
+            }
+        }
+    }
+}
+
